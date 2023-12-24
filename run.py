a83fed9630a30e8d4cfc8c8f869a17111d96b315
german deev <geradeev57@gmail.com>
2023-12-24 18:22:06 +0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,27 @@
+import asyncio
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher
+
+from app.handlers import router
+from app.database.models import async_main
+from config import TOKEN
+
+
+async def main():
+    await async_main()
+
+    bot = Bot(token=TOKEN, parse_mode='HTML')
+    dp = Dispatcher()
+    dp.include_router(router=router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
